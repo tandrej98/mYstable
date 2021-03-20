3b73252cb0e7caae60200ae2f0510b921708bdf7
elis4265 <alicaondreakova@gmail.com>
2021-03-20 19:01:24 +0100

    removed class registry, removed outdated TODOs

--- test_tree.py
+++ test_tree.py
@@ -70,3 +70,4 @@ class TestNameSpace(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+    
--- tree.py
+++ tree.py
@@ -25,14 +25,14 @@ class NameSpace(object):
             return index
         return self.name_index_map[name]
 
-    def space_add(self, s_name, *paths):
+    def space_add(self, s_name, *paths): #TODO add also space  and all its paths to mask set, /
         space_id = self.space(s_name)
         self.nodes[space_id].update(paths)
         for pth in paths:
             node = self._node_for_path(pth)
             node.worlds_mask_set(space_id)
 
-    def space_remove(self, s_name, *paths):
+    def space_remove(self, s_name, *paths): #TODO add also space() and all its paths to mask set, /
         space_id = self.space(s_name)
         self.nodes[space_id].difference_update(paths)
         for pth in paths:
@@ -45,7 +45,7 @@ class NameSpace(object):
             self._node_for_path(path).worlds_mask_test(space_id) for path in paths
         )
        
-    def _node_for_path(self, pth): #TODO node traverse not working 
+    def _node_for_path(self, pth):
         node = self.tree.get_node(pth)
         if node is None:
             components = pth.split('/')
@@ -76,133 +76,3 @@ class Node:
         self.worlds_mask = self.worlds_mask & ~(1 << space_id)
 
 
-class Register():
-    def __init__(self):
-        self.hooks = {}
-
-    def __call__(self, evname, **kwargs):
-        def register_decorator(func):
-            hooks = self.hooks.setdefault(evname, [])
-            hooks.append({'exec': func,
-                          'event': kwargs.get('event'),
-                          'object': kwargs.get('object'),
-                          'subject': kwargs.get('subject')})
-            return func
-        return register_decorator
-
-
-def exec(cmd, obj):
-    # execute function
-    if callable(cmd):
-        return cmd(obj)
-    # conpare dictionaries
-    if isinstance(cmd, dict):
-        #print('compare dictionaries')
-        for k, v in cmd.items():
-            if not exec(v, obj.__getattr__(k)):
-                False
-        return True
-    if isinstance(cmd, list):
-        # compare lists
-        if isinstance(obj, list):
-            if len(cmd) != len(obj):
-                return False
-            for i in range(len(cmd)):
-                if not exec(cmd[i], obj.__getattr__(i)):
-                    return False
-            return True
-        # this is OR
-        for i in cmd:
-            if exec(i, obj):
-                return True
-        return False
-    # and compare
-    return cmd == obj
-
-
-class Xor:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-
-    def __call__(self, obj):
-        return exec(self.a, obj) ^ exec(self.b, obj)
-
-
-class Not:
-    def __init__(self, arg):
-        self.arg = arg
-
-    def __call__(self, val):
-        return not exec(self.arg, val)
-
-
-class And:
-    def __init__(self, *args):
-        self.args = args
-
-    def __call__(self, obj):
-        for i in self.args:
-            if not exec(i, obj):
-                return False
-        return True
-
-
-class Or:
-    def __init__(self, *args):
-        self.args = args
-
-    def __call__(self, obj):
-        for i in self.args:
-            if exec(i, obj):
-                return True
-        return False
-
-
-class Dividable:
-    def __init__(self, val):
-        self.val = val
-
-    def __call__(self, val):
-        return self.val % val == 0
-
-
-class Ge:
-    def __init__(self, val):
-        self.val = val
-
-    def __call__(self, val):
-        return self.val <= val
-
-
-class Gt:
-    def __init__(self, val):
-        self.val = val
-
-    def __call__(self, val):
-        return self.val < val
-
-
-class Le:
-    def __init__(self, val):
-        self.val = val
-
-    def __call__(self, val):
-        return self.val >= val
-
-
-class Lt:
-    def __init__(self, val):
-        self.val = val
-
-    def __call__(self, val):
-        return self.val > val
-
-
-class Between:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-
-    def __call__(self, val):
-        return self.a <= val and val <= self.b
